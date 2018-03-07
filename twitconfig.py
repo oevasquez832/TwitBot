ca01178f8b6ead71638ac6f5a588fc5a67537d89
Oscar Eli Vasquez <oevasquez832@gmail.com>
2018-03-06 20:41:26 -0600

    Delete twitconfig.py

--- twitconfig.py
+++
@@ -1,4 +0,0 @@
-api_key = 'Ug0liKPGJeBO8DUKlVsWFEke3'
-api_secret = 'idwb1dkQvw3FlD1gHxEvlcuHBNI2GCGc26N2aDBgAoVvOMc6Du'
-access_token = '971036516947554305-JkxK0ua4Hz47V5YHhnBUOHDM3cjoYd4'
-token_secret = 'ImzP4sBh0a0PWuVrzJ6WkOU1a43FEDi01jnK9xcuqTXHh'          
